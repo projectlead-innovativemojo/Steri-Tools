2673b1004443eb06184118792045ef998000cb0e
alihassamdev <hassam.ali@studio93.io>
2025-09-09 15:24:47 +0500

    feat( Home: Why Now Section ) Added Home Why Not Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import HomeHero from "@/components/Home/Hero";
 import SurgicalInstruments from "@/components/Home/Surgical-Instruments";
+import WhyNow from "@/components/Home/Why-Now";
 
 const HomePage = () => {
   return (
     <>
       <HomeHero />
       <SurgicalInstruments />
+      <WhyNow />
     </>
   );
 };
