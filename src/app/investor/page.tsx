16767de8370046374b516c6451b53bae67e29b15
alihassamofficial <hassamali.official@gmail.com>
2025-09-10 15:40:43 +0500

    feat( Investor: Investor Hero Section ) Added The Investor Hero Section

---
+++ src/app/investor/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import InvestorHero from "@/components/Investor/Hero";
+
+const InvestorPage = () => {
+  return (
+    <>
+      <InvestorHero />
+    </>
+  );
+};
+
+export default InvestorPage;
