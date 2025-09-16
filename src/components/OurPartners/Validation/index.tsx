741553122e75c8b24f0d0e16f02a17e052158bd6
alihassamofficial <hassamali.official@gmail.com>
2025-09-16 17:25:05 +0500

    (Our Patners:) updtaed validation section, added open to tab content, remove text content from section, add z-index

--- src/components/OurPartners/Validation/index.tsx
+++ src/components/OurPartners/Validation/index.tsx
@@ -13,6 +13,8 @@ import blueYellowRing from "@/public/images/partners/single-yellow-blue-ring.svg
 
 const Validation = () => {
   const [showContent, setShowContent] = useState(false);
+  const [showNorthsky, setShowNorthsky] = useState(false);
+
   return (
     <div className="w-full max-w-[1237px] mx-auto px-5">
       <Text as="h2" className="mb-[57px] md:mb-[85px] text-center">
@@ -20,7 +22,7 @@ const Validation = () => {
       </Text>
 
       {/* test labs */}
-      <div className="relative w-full bg-[#14205A] rounded-[24px] mb-[54px] md:mb-[73px] min-h-[260px] flex flex-col md:flex-row px-4">
+      <div className="relative w-full bg-[#14205A] rounded-[24px] mb-[108px] md:mb-[146px]  px-4">
         {/* Yellow Rings BG */}
         <Image
           src={yellowDoubleRing}
@@ -33,8 +35,8 @@ const Validation = () => {
           "
         />
 
-        <div className="w-full max-w-[987px] mx-auto md:flex-row flex-col flex justify-between items-center">
-          <div className="flex gap-[49px] md:flex-row flex-col items-center py-[30px]">
+        <div className="w-full md:max-w-[987px] mx-auto flex justify-center md:justify-between items-center min-h-[260px] z-10">
+          <div className="flex gap-[49px] items-center py-[30px] md:flex-row flex-col z-10">
             <Image
               src={testlabs}
               alt="eee"
@@ -46,14 +48,58 @@ const Validation = () => {
               Regulatory Testing Partner
             </Text>
           </div>
-          <div className="max-w-[190px] w-full pb-[30px] md:pb-0">
-            <Button
-              onclick={() => setShowContent(!showContent)}
-              className="w-[190px] h-[50px]"
-            >
-              Read More
-            </Button>
-          </div>
+          <Button
+            onclick={() => setShowContent(!showContent)}
+            className="w-[190px] h-[50px]  md:flex hidden  pb-[30px] md:pb-0"
+          >
+            {showContent ? "Read Less" : "Read More"}
+          </Button>
+        </div>
+        {showContent && (
+          <>
+            <hr className="border-[0.5px] border-[#EDD98A] mb-[33px] w-full max-w-[987px] mx-auto hidden md:block" />
+            <Text className="pb-[51px] text-[#FFFFFF] w-full max-w-[264px] md:max-w-[987px] mx-auto ">
+              TestLabs UK is a GLP-certified, ISO 17025-accredited medical
+              device laboratory that specializes in delivering complete
+              validation services for international regulatory approvals. With
+              expertise spanning microbiology, materials science, and electrical
+              safety, TestLabs provides the full suite of testing required for
+              FDA 510(k) and CE Mark submissions.
+              <br />
+              <br />
+              Role in SteriTools:
+              <br />
+              TestLabs serves as SteriTools’ end-to-end validation partner,
+              responsible for:
+              <br />
+              ● Antimicrobial efficacy testing – validating SteriBasinGOTM’s
+              7-log reduction performance against FDA identified SSI-related
+              pathogens. <br />
+              ● Aerosolization testing – assessing airborne particle dispersion
+              and contamination control in surgical settings. <br />
+              ● Sterilization and disinfection validation – verifying repeatable
+              effectiveness across diverse surgical instruments and use cycles.
+              <br />
+              ● Electronics and safety testing – evaluating electrical safety,
+              EMC, and compliance with IEC standards. <br />● Material
+              compatibility and durability studies – confirming device integrity
+              under repeated sterilization and real-world use conditions.
+              <br />
+              <br />
+              By providing comprehensive, regulatory-grade data packages,
+              TestLabs ensures, SteriTools’ devices are fully validated for FDA
+              and CE submission, de-risking approval and enhancing confidence
+              with both regulators and clinical partners.
+            </Text>
+          </>
+        )}
+        <div className="">
+          <Button
+            onclick={() => setShowContent(!showContent)}
+            className="w-[190px]  h-[50px] md:hidden flex mx-auto "
+          >
+            {showContent ? "Read Less" : "Read More"}
+          </Button>
         </div>
 
         {/* BlueYellow Rings BG */}
@@ -69,22 +115,8 @@ const Validation = () => {
         />
       </div>
 
-      <Text className="mb-[57px] md:mb-[73px] text-center font-medium font-montserrat text-[18px] md:text-[22px] md:leading-[28px]">
-        By providing comprehensive, regulatory-grade data packages, TestLabs
-        ensures,{" "}
-        <span className="text-[#EDD98A] font-bold"> {`SteriTools’`} </span>{" "}
-        devices are fully validated for{" "}
-        <span className="text-[#EDD98A] font-bold">
-          {" "}
-          FDA and CE submission,{" "}
-        </span>{" "}
-        de-risking approval and enhancing confidence with both regulators and
-        clinical partners.
-      </Text>
-
       {/* northsky */}
-
-      <div className="relative w-full bg-[#14205A] rounded-[24px] mb-[54px] md:mb-[73px] min-h-[260px] flex flex-col md:flex-row px-4">
+      <div className="relative w-full bg-[#14205A] rounded-[24px] mb-[159px] md:mb-[153px]  px-4">
         {/* Yellow Rings BG */}
         <Image
           src={yellowDoubleRing}
@@ -97,8 +129,8 @@ const Validation = () => {
           "
         />
 
-        <div className="w-full max-w-[987px] mx-auto md:flex-row flex-col flex justify-between items-center">
-          <div className="flex gap-[49px] items-center py-[30px] md:flex-row flex-col">
+        <div className="w-full md:max-w-[987px] mx-auto flex justify-center md:justify-between items-center min-h-[260px] z-10">
+          <div className="flex gap-[49px] items-center py-[30px] md:flex-row flex-col z-10">
             <Image
               src={northsky}
               alt="northsky"
@@ -109,14 +141,50 @@ const Validation = () => {
               Engineering & Manufacturing Partner
             </Text>
           </div>
-          <div className="max-w-[190px] w-full pb-[30px] md:pb-0">
-            <Button
-              onclick={() => setShowContent(!showContent)}
-              className="w-[190px] h-[50px]"
-            >
-              Read More
-            </Button>
-          </div>
+          <Button
+            onclick={() => setShowNorthsky(!showNorthsky)}
+            className="w-[190px] h-[50px]  md:flex hidden pb-[30px] md:pb-0"
+          >
+            {showNorthsky ? "Read Less" : "Read More"}
+          </Button>
+        </div>
+
+        {showNorthsky && (
+          <>
+            <hr className="border-[0.5px] border-[#EDD98A] mb-[33px] w-full max-w-[987px] mx-auto hidden md:block" />
+            <Text className="pb-[51px] text-[#FFFFFF] w-full max-w-[264px] md:max-w-[987px] mx-auto ">
+              NorthSky Design Limited, based in Sheffield, UK, is a
+              healthcare-focused product design and engineering firm with deep
+              expertise in design-for-manufacturing (DFM) and commercialization.
+              <br />
+              <br />
+              Role in SteriTools: <br />
+              NorthSky is SteriTools’ integrated design and engineering partner,
+              overseeing: <br />
+              ● MVP product engineering – turning SteriBasinGOTM from concept
+              into functional, regulatory-ready prototypes. <br />
+              ● Design for manufacturability (DFM) – ensuring SteriTools
+              products are scalable, cost-efficient, and production-ready from
+              day one. <br />
+              ● Prototype-to-production scaling – managing the handoff from
+              pilot runs to volume manufacturing. <br />● Supply chain
+              integration – leveraging international manufacturing networks
+              across the UK, Europe, Asia, and North America.
+              <br />
+              <br />
+              NorthSky ensures SteriBasinGOTM is clinically effective,
+              ergonomically optimized, and scalable for both defense and
+              civilian markets.
+            </Text>
+          </>
+        )}
+        <div className="">
+          <Button
+            onclick={() => setShowNorthsky(!showNorthsky)}
+            className="w-[190px]  h-[50px] md:hidden flex mx-auto "
+          >
+            {showNorthsky ? "Read Less" : "Read More"}
+          </Button>
         </div>
 
         {/* BlueYellow Rings BG */}
@@ -131,13 +199,6 @@ const Validation = () => {
           "
         />
       </div>
-
-      <Text className="mb-[105px] md:mb-[73px] text-center font-medium font-montserrat text-[18px] md:text-[22px] md:leading-[28px]">
-        NorthSky ensures{" "}
-        <span className="text-[#EDD98A] font-bold"> SteriBasinGOTM </span> is
-        clinically effective, ergonomically optimized, and scalable for both
-        defense and civilian markets
-      </Text>
     </div>
   );
 };
